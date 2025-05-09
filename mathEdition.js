51e23bf8e44a1df7d2484cc5c61bfdac60977522
A-dawg19 <undant19@ecolecatholique.ca>
2025-05-08 21:06:01 -0400

    Update mathEdition.js

--- mathEdition.js
+++ mathEdition.js
@@ -2,7 +2,14 @@
   var total = 0
 function clickCounter() {
   total++
-  var affiche = document.getElementById("counter");
-  affiche.innerHTML = total;
-  
+  var affiche1 = document.getElementById("counter");
+  affiche1.innerHTML = total;
 }
+
+var sclick = 0
+function parSeconde () {
+    sclick++
+    var affiche2 = document.getElementById("counter");
+    affiche2.innerHTML = sclick;
+}
+setInterval(parSeconde, 1000)
